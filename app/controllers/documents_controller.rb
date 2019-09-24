a6e3eb38ed5ffcddcb80b8745f467ccbea87e665
Andrew Mason <andrewmcodes@protonmail.com>
2019-09-24 16:09:40 -0400

    setup actiontext

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -8,7 +8,6 @@ class DocumentsController < ApplicationController
   end
 
   # GET /documents/1
-  # GET /documents/1.json
   def show
   end
 
@@ -22,42 +21,34 @@ class DocumentsController < ApplicationController
   end
 
   # POST /documents
-  # POST /documents.json
   def create
     @document = Document.new(document_params)
 
     respond_to do |format|
       if @document.save
         format.html { redirect_to @document, notice: 'Document was successfully created.' }
-        format.json { render :show, status: :created, location: @document }
       else
         format.html { render :new }
-        format.json { render json: @document.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # PATCH/PUT /documents/1
-  # PATCH/PUT /documents/1.json
   def update
     respond_to do |format|
       if @document.update(document_params)
         format.html { redirect_to @document, notice: 'Document was successfully updated.' }
-        format.json { render :show, status: :ok, location: @document }
       else
         format.html { render :edit }
-        format.json { render json: @document.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /documents/1
-  # DELETE /documents/1.json
   def destroy
     @document.destroy
     respond_to do |format|
       format.html { redirect_to documents_url, notice: 'Document was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
